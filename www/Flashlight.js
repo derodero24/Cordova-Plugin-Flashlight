66b22674b2117d47c6fafaa457bb4d5cc1a5ae57
derodero24 <k.itsh20524abc@gmail.com>
2022-04-06 11:44:53 +0900

    Change to use Promise instead of Callback

--- www/Flashlight.js
+++ www/Flashlight.js
@@ -1,47 +1,50 @@
-function Flashlight() {
-  // track flashlight state
-  this._isSwitchedOn = false;
-}
-
-Flashlight.prototype = {
-
-  available: function (callback) {
-    cordova.exec(function (avail) {
-      callback(avail ? true : false);
-    }, function() { callback(false); }, "Flashlight", "available", []);
-  },
-
-  switchOn: function (successCallback, errorCallback, options) {
-    var opts = options || {};
-    this._isSwitchedOn = true;
-    cordova.exec(successCallback, errorCallback, "Flashlight", "switchOn", [opts]);
-  },
-
-  switchOff: function (successCallback, errorCallback) {
-    this._isSwitchedOn = false;
-    cordova.exec(successCallback, errorCallback, "Flashlight", "switchOff", []);
-  },
-
-  toggle: function (successCallback, errorCallback, options) {
-    if (this._isSwitchedOn) {
-      this.switchOff(successCallback, errorCallback);
-    } else {
-      this.switchOn(successCallback, errorCallback, options);
+"use strict";
+var Flashlight = /** @class */ (function () {
+    function Flashlight() {
+        this._isSwitchedOn = false;
     }
-  },
-
-  isSwitchedOn: function () {
-    return this._isSwitchedOn;
-  }
-};
-
-Flashlight.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-
-  window.plugins.flashlight = new Flashlight();
-  return window.plugins.flashlight;
-};
-
-cordova.addConstructor(Flashlight.install);
+    Flashlight.prototype.available = function () {
+        return new Promise(function (resolve, reject) {
+            cordova.exec(function (result) { return resolve(result ? true : false); }, function () { return reject(); }, 'Flashlight', 'available', []);
+        });
+    };
+    Flashlight.prototype.isSwitchedOn = function () {
+        return this._isSwitchedOn;
+    };
+    Flashlight.prototype.switchOn = function (options) {
+        var _this = this;
+        var opts = options || {};
+        return new Promise(function (resolve, reject) {
+            cordova.exec(function () {
+                _this._isSwitchedOn = true;
+                resolve();
+            }, reject, 'Flashlight', 'switchOn', [opts]);
+        });
+    };
+    Flashlight.prototype.switchOff = function () {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            cordova.exec(function () {
+                _this._isSwitchedOn = false;
+                resolve();
+            }, reject, 'Flashlight', 'switchOff', []);
+        });
+    };
+    Flashlight.prototype.toggle = function (options) {
+        if (this._isSwitchedOn) {
+            return this.switchOff();
+        }
+        else {
+            return this.switchOn(options);
+        }
+    };
+    return Flashlight;
+}());
+// plugin installer
+cordova.addConstructor(function () {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+    window.plugins.flashlight = new Flashlight();
+    return window.plugins.flashlight;
+});
---
+++ www/Flashlight.ts
@@ -0,0 +1,84 @@
+declare const cordova: {
+  exec: (
+    successCallback: (_: any) => void,
+    errorCallback: () => void,
+    pluginName: string,
+    actionName: string,
+    options: any[]
+  ) => {};
+  addConstructor: (installer: () => void) => void;
+};
+
+declare const window: any;
+
+class Flashlight {
+  _isSwitchedOn: boolean;
+
+  constructor() {
+    this._isSwitchedOn = false;
+  }
+
+  available(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        (result: number) => resolve(result ? true : false),
+        () => reject(),
+        'Flashlight',
+        'available',
+        []
+      );
+    });
+  }
+
+  isSwitchedOn(): boolean {
+    return this._isSwitchedOn;
+  }
+
+  switchOn(options?: object): Promise<void> {
+    let opts = options || {};
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        () => {
+          this._isSwitchedOn = true;
+          resolve();
+        },
+        reject,
+        'Flashlight',
+        'switchOn',
+        [opts]
+      );
+    });
+  }
+
+  switchOff(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        () => {
+          this._isSwitchedOn = false;
+          resolve();
+        },
+        reject,
+        'Flashlight',
+        'switchOff',
+        []
+      );
+    });
+  }
+
+  toggle(options?: object): Promise<void> {
+    if (this._isSwitchedOn) {
+      return this.switchOff();
+    } else {
+      return this.switchOn(options);
+    }
+  }
+}
+
+// plugin installer
+cordova.addConstructor(() => {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.flashlight = new Flashlight();
+  return window.plugins.flashlight;
+});
